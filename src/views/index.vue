2fb65de2736b9863b3afd0037b3330d4be29ad4d
pp_lan <zgjszhuhui@163.com>
2024-09-28 15:05:23 +0800

    logo

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,9 +3,32 @@
 </script>
 
 <template>
-  首页 - dengruicode.com
+  <div class="title">
+    <img alt="" class="logo" src="@/assets/images/essays.jpeg">
+    <span>随笔</span>
+  </div>
 </template>
 
 <style scoped>
+  .title {
+    padding: 20px 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: inherit;
+    width: 100%;
+    font-size: 50px;
+    color: #a84b4b;
+    font-family: 华文楷体;
 
+    span {
+      margin-left: 20px;
+    }
+  }
+  .logo {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
 </style>
