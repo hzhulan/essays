6bb7a8001d71a6db5a009aa55315e2bfa01c20e5
pp_lan <zgjszhuhui@163.com>
2024-09-28 16:23:32 +0800

    logo

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,35 +1,38 @@
 <script setup>
+const title = "随笔"
+const id = "title"
 
 </script>
 
 <template>
   <div class="title">
     <img alt="" class="logo" src="@/assets/images/essays.jpeg">
-    <span>随笔</span>
+    <span :id>{{ title }}</span>
   </div>
 </template>
 
 <style scoped>
-  .title {
-    box-sizing: border-box;
-    padding: 20px 20px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    height: inherit;
-    width: 100%;
-    font-size: 50px;
-    color: #a84b4b;
-    font-family: 华文楷体;
+.title {
+  box-sizing: border-box;
+  padding: 20px 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: inherit;
+  width: 100%;
+  font-size: 50px;
+  color: #a84b4b;
+  font-family: 华文楷体;
 
-    span {
-      margin-left: 20px;
-    }
+  span {
+    margin-left: 20px;
   }
-  .logo {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-  }
+}
+
+.logo {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
 </style>
