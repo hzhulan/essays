6f71b6122eece047a8769165f638134ed372ac57
pp_lan <zgjszhuhui@163.com>
2024-11-18 09:38:38 +0800

    打包脚本

--- package.bat
+++ package.bat
@@ -1,9 +1,11 @@
 @echo off
+
 set zipPath=D:\system\7-Zip\
 set zipExe=%zipPath%/7z.exe
+echo current path: %zipExe%
 
-echo "current path: %zipExe%"
+echo start package process...
 %zipExe% a -tzip "D:\workspace\front\essays\dist.zip" "D:\workspace\front\essays\dist"
+echo package finished.
 pause
-echo "package finished"
 exit
